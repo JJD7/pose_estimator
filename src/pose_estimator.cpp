313a633b36d50ee3f6e3edee71c5a88726af5573
James Donnelly <jd7@vt.edu>
2018-10-31 08:23:21 -0400

    added source files

---
+++ src/pose_estimator.cpp
@@ -0,0 +1,11 @@
+#include "pose_estimator.h"
+#include <iostream>
+
+pose_estimator::pose_estimator()
+{
+    std::cout << "Hello World!";
+}
+
+int main(){
+    std::cout << "Hello World!";
+}
---
+++ src/pose_estimator.h
@@ -0,0 +1,11 @@
+#ifndef POSE_ESTIMATOR_H
+#define POSE_ESTIMATOR_H
+
+
+class pose_estimator
+{
+public:
+    pose_estimator();
+};
+
+#endif // POSE_ESTIMATOR_H
