f7dff13f5a9789185a13bfbdfd005c7092324ab9
James Donnelly <jd7@vt.edu>
2018-10-31 20:43:09 -0400

    Basic image subriber added for raw rectified images

--- src/pose_estimator.cpp
+++ src/pose_estimator.cpp
@@ -1,11 +1,51 @@
 #include "pose_estimator.h"
-#include <iostream>
 
-pose_estimator::pose_estimator()
-{
-    std::cout << "Hello World!";
+
+pose_estimator::pose_estimator(ros::NodeHandle* nodehandle):nh_(*nodehandle)
+{ // constructor
+    ROS_INFO("in class constructor of ExampleRosClass");
 }
 
-int main(){
-    std::cout << "Hello World!";
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::waitKey(30);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv){
+    // required to access ros. If this gives an error, make sure you are running
+    // roscore on your machine.
+    ros::init(argc,argv,"pose_estimator");
+    ros::NodeHandle nh;
+    ros::Duration(.1).sleep();
+
+
+    cv::namedWindow("left_rectified_image");
+    cv::startWindowThread();
+    ROS_INFO("Initializing Subscribers");
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber leftRect_image= it.subscribe("/left_right/left_rect/image_raw", 1, imageCallback);
+
+
+    ros::Publisher pose_estimate;
+    ROS_INFO("Initializing Publishers");
+    pose_estimate = nh.advertise<geometry_msgs::Pose>("estimated_pose",1);
+
+    ROS_INFO("Running");
+
+    while(ros::ok()){
+
+            ros::spinOnce();
+
+    }
+
+    cv::destroyWindow("left_rectified_image");
+    return 0;
 }
--- src/pose_estimator.h
+++ src/pose_estimator.h
@@ -1,11 +1,32 @@
 #ifndef POSE_ESTIMATOR_H
 #define POSE_ESTIMATOR_H
 
+#include <iostream>
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <tf/transform_listener.h>
+#include <tf/tf.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/Float32.h>
+#include <math.h>
+#include <string>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 class pose_estimator
 {
-public:
-    pose_estimator();
+    public:
+    pose_estimator(ros::NodeHandle* nodehandle);
+    void imgCallback(const  sensor_msgs::ImageConstPtr& msg);
+
+    private:
+    ros::NodeHandle nh_;
+    geometry_msgs::Pose est_pose;
+
 };
 
 #endif // POSE_ESTIMATOR_H
